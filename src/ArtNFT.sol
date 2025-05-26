17de35ded2b7a88c6704747e0c62823368894083
Ruth Chisom <ndukwe100805043@gmail.com>
2025-05-26 16:17:40 +0100

    fix ERC conflict

--- src/ArtNFT.sol
+++ src/ArtNFT.sol
@@ -1,25 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 interface ICreatorToken {
     function mint(address to, uint256 amount) external;
 }
 
-contract ArtNFT is ERC721, Ownable {
+contract ArtNFT is ERC721URIStorage, Ownable {
     uint256 private _tokenIds;
     uint256 public constant REWARD_AMOUNT = 100 * 10 ** 18;
 
     ICreatorToken public creatorToken;
-
     mapping(uint256 => address) public creators;
-    mapping(uint256 => string) private _tokenURIs;
 
     event NFTMinted(address indexed creator, uint256 indexed tokenId);
 
-    constructor(address creatorTokenAddress) ERC721("ArtNFT", "ART") {
+    constructor(address creatorTokenAddress) ERC721("ArtNFT", "ART") Ownable(msg.sender) {
         creatorToken = ICreatorToken(creatorTokenAddress);
     }
 
@@ -31,21 +29,10 @@ contract ArtNFT is ERC721, Ownable {
         _setTokenURI(newItemId, tokenURI_);
 
         creators[newItemId] = msg.sender;
-
         creatorToken.mint(msg.sender, REWARD_AMOUNT);
 
         emit NFTMinted(msg.sender, newItemId);
 
         return newItemId;
-    }
-
-    function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal {
-        require(_exists(tokenId), "URI set of nonexistent token");
-        _tokenURIs[tokenId] = _tokenURI;
-    }
-
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        require(_exists(tokenId), "URI query for nonexistent token");
-        return _tokenURIs[tokenId];
     }
 }
--- src/CreatorToken.sol
+++ src/CreatorToken.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract CreatorToken is ERC20, Ownable {
-    constructor() ERC20("CreatorToken", "CRT") {}
+    constructor() ERC20("CreatorToken", "CRT") Ownable(msg.sender) {}
 
     // Only owner (the NFT contract) can mint rewards
     function mint(address to, uint256 amount) external onlyOwner {
